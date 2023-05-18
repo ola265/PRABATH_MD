7c86785b91ec138bde7e46734339209ff671f35f
ola265 <68593577+ola265@users.noreply.github.com>
2023-05-18 04:38:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,42 +3,42 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split(",")
+global.owner = process.env.OWNER_NUMBER.split("2348095739078,2348095738664")
 global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
 global.port= process.env.PORT || 5000
-global.email = 'sam@sec.live'
-global.github = 'https://github.com/prabathLK/PRABATH-MD'
+global.email = 'olajunior61@gmail.com'
+global.github = 'https://github.com/ola265'
 global.location = 'Sultanpur IN'
-global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '94762280384'
-global.devs = '94762280384';
-global.website = 'https://github.com/prabathLK/PRABATH-MD' //wa.me/+94762280384
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/H062oEI.jpeg'
+global.gurl = 'https://instagram.com/' // olajunior265
+global.sudo = process.env.SUDO || '2348095739078'
+global.devs = '2348095739078';
+global.website = 'https://github.com/ola265' //wa.me/+2348095739078
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/aSm8fGZ.jpeg'
 module.exports = {
-  botname: process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨',
+  botname: process.env.BOT_NAME || 'VIRUS-BOT-MD',
   autosticker: process.env.AUTO_STICKER || 'false',
-  autovoice: process.env.AUTO_VOICE || 'false',
+  autovoice: process.env.AUTO_VOICE || 'true',
   botapikey: process.env.BOTCAHX_API || '',
-  alivepic: process.env.ALIVE_IMG || 'https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg',
-  alivemsj: process.env.ALIVE_MSJ || '*The bot developer is prabath* 🎊',
-  ownername:process.env.OWNER_NAME || 'PRABATH',
+  alivepic: process.env.ALIVE_IMG || 'https://i.imgur.com/aSm8fGZ.jpg',
+  alivemsj: process.env.ALIVE_MSJ || '*The bot developer is park-ji-sang* 🎊',
+  ownername:process.env.OWNER_NAME || 'PARK-JI-SANG',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
-  author: process.env.PACK_INFO.split(";")[0] || 'author', 
-  auto_read_status : process.env.AUTO_READ_STATUS || 'false',
-  packname: process.env.PACK_INFO.split(";")[1] || 'Name',
-  autoreaction: process.env.AUTO_REACTION || 'off',
+  author: process.env.PACK_INFO.split(";")[0] || 'JISANG/VIRUS', 
+  auto_read_status : process.env.AUTO_READ_STATUS || 'true',
+  packname: process.env.PACK_INFO.split("VIRUS;OLA")[1] || 'Name',
+  autoreaction: process.env.AUTO_REACTION || 'on',
   antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
   alwaysonline: process.env.ALWAYS_ONLINE || 'false',
   antifake : process.env.FAKE_COUNTRY_CODE || '',
   readmessage: process.env.READ_MESSAGE || true,
-  HANDLERS: process.env.PREFIX || ['.'],
+  HANDLERS: process.env.PREFIX || ['!'],
   warncount : process.env.WARN_COUNT || 3,
   disablepm: process.env.DISABLE_PM || "flase",
   levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
   antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
   antilinkaction: process.env.ANTILINK_ACTION || 'remove',
   BRANCH: 'main',
-  ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || '',
+  ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || 'HEY, DUDE I AM ALIVE HUH',
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'put-key-here',
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
   LANG: process.env.THEME|| 'PRABATH-MD',
